fa01138166e48cbcb9f64fb4109341bdf9e2c730
iasminguimaraes-droid <iasmin.fernandes@escola.pr.gov.br>
2026-07-24 21:25:41 -0300

    Update questao-3-a.c

--- questao-3-a.c
+++ questao-3-a.c
@@ -1,12 +1,12 @@
 int somaDigitos(int b) {
-  if (b == 0) {
-      return 0;
-  }
-  return (b % 10) + somaDigitos(b / 10);
+  	if (b == 0) {
+      	return 0;
+  	}
+	return (b % 10) + somaDigitos(b / 10);
 }
 
 int main()
 {
-  int resultado = somaDigitos(472);
+  	int resultado = somaDigitos(472);
 	return 0;
 }
