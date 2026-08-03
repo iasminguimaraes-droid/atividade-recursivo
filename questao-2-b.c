d220b3b8a1f2919c892f20739c39da45bb96df7a
iasminguimaraes-droid <iasmin.fernandes@escola.pr.gov.br>
2026-08-03 11:16:06 -0300

    Update questao-2-b.c

--- questao-2-b.c
+++ questao-2-b.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int fatorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * fatorial(n - 1);
+}
+
+int main() {
+    printf("%d", fatorial(4));
+    return 0;
+}
