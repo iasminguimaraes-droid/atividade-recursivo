b565fdf7375193912f55b724b2503409c338164f
iasminguimaraes-droid <iasmin.fernandes@escola.pr.gov.br>
2026-08-03 11:24:46 -0300

    Update questao-4-b.c

--- questao-4-b.c
+++ questao-4-b.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
 
+int buscaBinaria(int v[], int n, int alvo) {
+    int inicio = 0;
+    int fim = n - 1;
+
+    while (inicio <= fim) {
+       
+        int meio = inicio + (fim - inicio) / 2;
+
+        if (v[meio] == alvo) {
+            return meio; 
+        }
+        
+        if (v[meio] < alvo) {
+            inicio = meio + 1; 
+        } else {
+            fim = meio - 1;    
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int v[] = {1, 3, 5, 7, 9};
+    int n = 5;
+    int alvo = 5;
+
+    int resultado = buscaBinaria(v, n, alvo);
+
+    if (resultado != -1) {
+        printf("Elemento %d encontrado no índice: %d\n", alvo, resultado);
+    } else {
+        printf("Elemento %d não encontrado no vetor.\n", alvo);
+    }
+
+    return 0;
+}
