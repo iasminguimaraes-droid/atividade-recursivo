2906bd32a48164a231aa20c72e5449e1e7aa955c
iasminguimaraes-droid <iasmin.fernandes@escola.pr.gov.br>
2026-08-03 11:21:57 -0300

    Update questao-4-a.c

--- questao-4-a.c
+++ questao-4-a.c
@@ -1,1 +1,4 @@
+a busca binária funciona reduzindo a lista pela metade a cada etapa. o pior caso acontece quando o elemento procurado está na 
+última possibilidade de busca ou quando ele nem está presente na lista. 
+nessa situação, o algoritmo precisa continuar dividindo o espaço de busca até não restar mais nenhum elemento para verificar.
 
